1b3000c1f7e62dc9a56ad86a82f550a5b6dae9c6
kvnloughead <kvnloughead@gmail.com>
2022-07-05 14:35:13 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+/* 1. Select a list of all the todo elements and assign it to a variable called todos */
+
+/* 2. Select the first button and assign it to a variable called newTodoButton */
+
+/* 3. Select the second button and assign it to a variable called clearTodosButton */
