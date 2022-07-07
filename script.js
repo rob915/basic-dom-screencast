9508eab21ccfaf94ee0bb27fffc1db0453918c02
kvnloughead <kvnloughead@gmail.com>
2022-07-07 15:29:04 -0400

    Start of setting-attributes screencast

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-/* 1. Select a list of all the todo elements and assign it to a variable called todos */
-
-/* 2. Select the first button and assign it to a variable called newTodoButton */
-
-/* 3. Select the second button and assign it to a variable called clearTodosButton */
+const todos = document.querySelectorAll('.todo');
+const newTodoButton = document.querySelector('.button_action_new-todo');
+const clearTodosButton = document.querySelector('.button_action_clear-todos');
