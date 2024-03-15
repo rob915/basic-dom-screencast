7db580fce6cd1123b98834c539efa02930ef436c
Robert_Rodriguez <rob915@gmail.com>
2024-03-15 13:54:46 -0700

    first stage script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-/* 1. Select a list of all the todo elements and assign it to a variable called todos */
+/* 1. Select a list of all the todo elements and assignl it to a variable called todos */
+
+const todos = document.querySelectorAll(".todo");
+console.log(todos);
 
 /* 2. Select the first button and assign it to a variable called newTodoButton */
 
+const newTodoButton = document.querySelector(".button_action_new-todo");
+
 /* 3. Select the second button and assign it to a variable called clearTodosButton */
+
+const clearTodosButton = document.querySelector(".button_action_clear-todos");
