8ea3c835b3bdb9f532e1205cd1260fd1acbcb26b
Robert_Rodriguez <rob915@gmail.com>
2024-03-18 16:02:04 -0700

    manipulating-css-classes complete

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-const todos = document.querySelectorAll('.todo');
-const newTodoButton = document.querySelector('.button_action_new-todo');
-const clearTodosButton = document.querySelector('.button_action_clear-todos');
+const todos = document.querySelectorAll(".todo");
+const newTodoButton = document.querySelector(".button_action_new-todo");
+const clearTodosButton = document.querySelector(".button_action_clear-todos");
+const avatar = document.querySelector(".profile__avatar");
+avatar.classList.add("profile__avatar_round");
