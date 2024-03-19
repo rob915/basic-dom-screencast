aed6b449e5dcf6148f89e2c56a9b1edc34a9dd21
Robert_Rodriguez <rob915@gmail.com>
2024-03-19 15:34:40 -0700

    updating content complete

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const todos = document.querySelectorAll('.todo');
 const newTodoButton = document.querySelector('.button_action_new-todo');
 const clearTodosButton = document.querySelector('.button_action_clear-todos');
+
+const nameElement = document.querySelector('.profile__name');
+nameElement.textContent = 'New Name';
